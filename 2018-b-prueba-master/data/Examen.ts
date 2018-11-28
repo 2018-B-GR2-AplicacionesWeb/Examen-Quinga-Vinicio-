73b4d0c34987d95dacc6f2e740f00822a3579c7d
davini10c <vinicio_520@hotmail.com>
2018-11-28 16:00:11 -0500

    examen

---
+++ 2018-b-prueba-master/data/Examen.ts
@@ -0,0 +1,125 @@
+
+declare var require;
+
+const fs = require('fs');
+const rxjs = require('rxjs');
+
+
+
+
+//inicializamos la bdd
+function inicializarBDD() {
+    return new Promise((resolve, reject) => {
+        fs.readFile('people.json', 'utf-8', (error, contenidoArchivo) => {
+            if (error) {
+                error: error;
+            }
+            else {
+                resolve({
+                    mensaje: 'BDD leida',
+                    bdd: JSON.parse(contenidoArchivo)
+                });
+            }
+        });
+    });
+}
+
+const arregloF = [];
+const arregloM = [];
+const arregloN = [];
+
+//## 1) Busque los tipos de "gender" en el arreglo `people.json`
+
+
+
+inicializarBDD()
+    .then(respuesta => {
+        respuesta.bdd.map(valor => {
+            switch (valor.gender) {
+                case 'female':
+                    arregloF.push(valor.gender);
+                    break;
+                case 'male':
+                    arregloM.push(valor.gender);
+                    break;
+                case 'n/a':
+                    arregloN.push(valor.gender);
+                    break;
+            }
+        });
+        console.log("Busque los tipos de \"gender\" en el arreglo `people.json`");
+        console.log(arregloF);
+        console.log(arregloM);
+        console.log(arregloN);
+    });
+
+
+const arregloB = [];
+const arregloY = [];
+const arregloR = [];
+const arregloBr = [];
+
+ 
+//## 2) Busque los tipos de "eye_color" en el arreglo `people.json`
+inicializarBDD()
+    .then(respuesta => { //bdd no reconoce
+        respuesta.bdd.map(valor => {
+            switch (valor.eyes_color) {
+                case 'blue':
+                    arregloB.push(valor.eyes_color);
+                    break;
+                case 'yellow':
+                    arregloY.push(valor.eyes_color);
+                    break;
+                case 'red':
+                    arregloR.push(valor.eyes_color);
+                    break;
+                case 'brown':
+                    arregloBr.push(valor.eye_color);
+                    break
+            }
+        });
+
+        console.log("Busque los tipos de \"eye_color\" en el arreglo `people.json`")
+        console.log(arregloB);
+        console.log(arregloY);
+        console.log(arregloR);
+        console.log(arregloBr);
+
+    });
+
+
+const arreglof = [];
+const arreglog = [];
+const arreglow = [];
+const arreglol = [];
+
+//## 3) Busque los tipos de "skin_color" en el arreglo `people.json`
+
+inicializarBDD()
+    .then(respuesta => { //bdd no reconoce
+        respuesta.bdd.map(valor => {
+            switch (valor.skin_color) {
+                case 'fair':
+                    arreglof.push(valor.skin_color);
+                    break;
+                case 'gold':
+                    arreglog.push(valor.skin_color);
+                    break;
+                case 'white':
+                    arreglow.push(valor.skin_color);
+                    break;
+                case 'light':
+                    arreglol.push(valor.skin_color);
+                    break
+            }
+        });
+
+        console.log("Busque los tipos de \"eye_color\" en el arreglo `people.json`")
+        console.log(arreglof);
+        console.log(arreglog);
+        console.log(arreglow);
+        console.log(arreglol);
+
+    });
+
